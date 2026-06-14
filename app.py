7bd1bca71801255cdeae9844f7a0cca9d4716a69
kanaerss <kristinalike658@gmail.com>
2026-06-15 02:48:54 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def schedule():
 if name == 'main':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
