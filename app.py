2702a92eede335ecaedb01a01387c3fe15b262ab
kanaerss <kristinalike658@gmail.com>
2026-06-15 02:43:36 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -20,5 +20,7 @@ def schedule():
     return render_template('schedule.html')
 
 # Запуск сервера (всегда в самом конце)
-if __name__ == '__main__':
-    app.run(debug=True)
+if name == 'main':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
